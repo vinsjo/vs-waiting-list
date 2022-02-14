3643de89c23ac49653015f08b5ab1b775bdcd427
VinSjo <vincentsjogren@gmail.com>
2022-02-14 13:59:23 +0100

    updated EntryTimer

--- src/components/EntryTimer.jsx
+++ src/components/EntryTimer.jsx
@@ -1,37 +1,30 @@
 import { useEffect, useState } from 'react';
 
+function elapsedTimeString(startTime) {
+	if (typeof startTime !== 'number') return '';
+	const elapsed = Math.max(Date.now() - startTime, 0);
+	function strOutput(val, unit) {
+		return `${val} ${unit}${val > 1 ? 's' : ''} ago`;
+	}
+	const h = Math.floor(elapsed / 3600000) || 0;
+	if (h >= 1) return strOutput(h, 'hour');
+	const m = Math.floor(elapsed / 60000) % 60 || 0;
+	if (m >= 1) return strOutput(m, 'minute');
+	return 'Less than a minute ago';
+}
+
 function EntryTimer({ start }) {
 	const [startTime] = useState(start || Date.now());
-	const [elapsedTime, setElapsedTime] = useState(
-		!startTime ? 0 : Math.max(Date.now() - startTime, 0)
-	);
-	const [output, setOutput] = useState('');
-
-	function getOutputString(ms) {
-		if (!ms) return '';
-		function strOutput(val, unit) {
-			return `${val} ${unit}${val > 1 ? 's' : ''} ago`;
-		}
-		const h = Math.floor(ms / 3600000) || 0;
-		if (h >= 1) return strOutput(h, 'hour');
-		const m = Math.floor(ms / 60000) % 60 || 0;
-		if (m >= 1) return strOutput(m, 'minute');
-		return 'Less than a minute ago';
-	}
-
-	useEffect(
-		() => setOutput(getOutputString(elapsedTime)),
-		[elapsedTime, setElapsedTime]
-	);
-
+	const [text, setText] = useState(() => elapsedTimeString(startTime));
 	useEffect(() => {
-		const timeout = setTimeout(() => {
-			setElapsedTime(Math.max(Date.now() - startTime, 0));
-		}, 1000);
+		const timeout = setTimeout(
+			() => setText(elapsedTimeString(startTime)),
+			1000
+		);
 		return () => clearTimeout(timeout);
-	}, [startTime, elapsedTime, setElapsedTime]);
+	}, [startTime, text, setText]);
 
-	return <>{output}</>;
+	return <>{text}</>;
 }
 
 export default EntryTimer;
